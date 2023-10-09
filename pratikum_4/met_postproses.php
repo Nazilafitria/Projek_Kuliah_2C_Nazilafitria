fa48b2e2315358912afaeb249b71a2dbce118849
Nazilafitria <112499349+Nazilafitria@users.noreply.github.com>
2023-10-09 21:30:11 +0700

    Add files via upload

---
+++ pratikum_4/met_postproses.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title> method POST proses </title>
+    </head>
+    <body>
+        alamat anda adalah : desa blang lhoksukon <?echo $_POST["alamat"]?> 
+    </body>
+</html>
