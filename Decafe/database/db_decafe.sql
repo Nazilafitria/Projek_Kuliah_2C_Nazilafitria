6788108322116b197ff1e46e3f6a631dc02c7132
Nazilafitria <112499349+Nazilafitria@users.noreply.github.com>
2023-11-14 02:05:15 +0700

    Add files via upload

---
+++ Decafe/database/db_decafe.sql
@@ -0,0 +1,139 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for db_decafe
+CREATE DATABASE IF NOT EXISTS `db_decafe` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE  */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_decafe`;
+
+-- Dumping structure for table db_decafe.tb_bayar
+CREATE TABLE IF NOT EXISTS `tb_bayar` (
+  `id_bayar` bigint NOT NULL,
+  `nominal_uang` bigint DEFAULT NULL,
+  `total_bayar` bigint DEFAULT NULL,
+  `waktu_bayar` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_bayar`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+-- Dumping data for table db_decafe.tb_bayar: ~0 rows (approximately)
+INSERT INTO `tb_bayar` (`id_bayar`, `nominal_uang`, `total_bayar`, `waktu_bayar`) VALUES
+	(2311131857922, 200000, 20000, '2023-11-13 16:49:48'),
+	(2311131915591, 100000, 10000, '2023-11-13 16:52:43');
+
+-- Dumping structure for table db_decafe.tb_daftar_menu
+CREATE TABLE IF NOT EXISTS `tb_daftar_menu` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `foto` varchar(200) CHARACTER SET utf8mb4 COLLATE  DEFAULT NULL,
+  `nama_menu` varchar(200) CHARACTER SET utf8mb4 COLLATE  DEFAULT NULL,
+  `keterangan` varchar(200) CHARACTER SET utf8mb4 COLLATE  DEFAULT NULL,
+  `kategori` int DEFAULT NULL,
+  `harga` varchar(100) CHARACTER SET utf8mb4 COLLATE  DEFAULT NULL,
+  `stok` varchar(50) CHARACTER SET utf8mb4 COLLATE  DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `FK_tb_daftar_menu_tb_kategori_menu` (`kategori`),
+  CONSTRAINT `FK_tb_daftar_menu_tb_kategori_menu` FOREIGN KEY (`kategori`) REFERENCES `tb_kategori_menu` (`id_kat_menu`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4  ROW_FORMAT=DYNAMIC;
+
+-- Dumping data for table db_decafe.tb_daftar_menu: ~7 rows (approximately)
+INSERT INTO `tb_daftar_menu` (`id`, `foto`, `nama_menu`, `keterangan`, `kategori`, `harga`, `stok`) VALUES
+	(1, '70679-1.png', 'Mie Acehh', 'Asli Aceh bukan kaleng kaleng', 2, '10000', '20'),
+	(2, '2.png', 'Burger', 'Burger ini mirip Burger king', 1, '5000', '4'),
+	(3, '3.png', 'Kari kambing', 'Kambing khusus dari india', 2, '15000', '6'),
+	(4, '4.png', 'Kopi Sanger', 'Yang buat cogan', 4, '20000', '14'),
+	(5, '5.png', 'Es Timun serut', 'Timun segar diparut dengan lembut', 3, '5000', '25'),
+	(6, '75912-6.png', 'Nasi Goreng Udang', 'Udang impor dari Korea', 1, '25000', '4'),
+	(8, '89267-8.png', 'Nasi Uduk Maklemak', 'Asli nasi sih', 1, '15000', '20');
+
+-- Dumping structure for table db_decafe.tb_kategori_menu
+CREATE TABLE IF NOT EXISTS `tb_kategori_menu` (
+  `id_kat_menu` int NOT NULL AUTO_INCREMENT,
+  `jenis_menu` int DEFAULT NULL,
+  `kategori_menu` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_kat_menu`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 ;
+
+-- Dumping data for table db_decafe.tb_kategori_menu: ~4 rows (approximately)
+INSERT INTO `tb_kategori_menu` (`id_kat_menu`, `jenis_menu`, `kategori_menu`) VALUES
+	(1, 1, 'Nasi'),
+	(2, 1, 'jajanan'),
+	(3, 2, 'Jus'),
+	(4, 2, 'Kopi');
+
+-- Dumping structure for table db_decafe.tb_list_order
+CREATE TABLE IF NOT EXISTS `tb_list_order` (
+  `id_list_order` int NOT NULL AUTO_INCREMENT,
+  `menu` int DEFAULT NULL,
+  `kode_order` bigint DEFAULT NULL,
+  `jumblah` int DEFAULT NULL,
+  `catatan` varchar(300) DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  PRIMARY KEY (`id_list_order`),
+  KEY `menu` (`menu`),
+  KEY `order` (`kode_order`) USING BTREE,
+  CONSTRAINT `FK_tb_list_order_tb_daftar_menu` FOREIGN KEY (`menu`) REFERENCES `tb_daftar_menu` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `FK_tb_list_order_tb_order` FOREIGN KEY (`kode_order`) REFERENCES `tb_order` (`id_order`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 ;
+
+-- Dumping data for table db_decafe.tb_list_order: ~4 rows (approximately)
+INSERT INTO `tb_list_order` (`id_list_order`, `menu`, `kode_order`, `jumblah`, `catatan`, `status`) VALUES
+	(1, 5, 2311131915591, 2, 'aaaa', 2),
+	(2, 1, 2311131857922, 2, 'pake cumi', 2),
+	(3, 2, 2311131902447, 1, 'bvgv', NULL),
+	(4, 8, 2311131938717, 2, 'jangan taruh kacang', 2);
+
+-- Dumping structure for table db_decafe.tb_order
+CREATE TABLE IF NOT EXISTS `tb_order` (
+  `id_order` bigint NOT NULL DEFAULT '0',
+  `pelanggan` varchar(200) DEFAULT NULL,
+  `meja` int DEFAULT NULL,
+  `pelayan` int DEFAULT NULL,
+  `waktu_order` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_order`),
+  KEY `pelayan` (`pelayan`),
+  CONSTRAINT `FK_tb_order_tb_user` FOREIGN KEY (`pelayan`) REFERENCES `tb_user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+
+-- Dumping data for table db_decafe.tb_order: ~4 rows (approximately)
+INSERT INTO `tb_order` (`id_order`, `pelanggan`, `meja`, `pelayan`, `waktu_order`) VALUES
+	(2311131857922, 'Nazilaa', 1, 2, NULL),
+	(2311131902447, 'Reza', 4, 2, '2023-11-13 13:24:44'),
+	(2311131915591, 'adam', 33, 2, NULL),
+	(2311131938717, 'Yohan', 6, 2, '2023-11-13 12:39:18');
+
+-- Dumping structure for table db_decafe.tb_user
+CREATE TABLE IF NOT EXISTS `tb_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(200) CHARACTER SET utf8mb4 COLLATE  DEFAULT NULL,
+  `username` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `level` int DEFAULT NULL,
+  `nohp` varchar(15) DEFAULT NULL,
+  `alamat` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 ;
+
+-- Dumping data for table db_decafe.tb_user: ~4 rows (approximately)
+INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `level`, `nohp`, `alamat`) VALUES
+	(1, 'Sheila', 'abc1@abc.com', '5f4dcc3b5aa765d61d8327deb882cf99', 4, '1234567891011', 'Lhokseumawe'),
+	(2, 'owner', 'admin@admin.com', '5f4dcc3b5aa765d61d8327deb882cf99', 1, '1234567891011', NULL),
+	(3, 'Nazila', 'abc2@abc.com', '5f4dcc3b5aa765d61d8327deb882cf99', 2, '1234567891011', ''),
+	(4, 'abc3', 'abc3@abc.com', '5f4dcc3b5aa765d61d8327deb882cf99', 4, '1234567891011', NULL);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
