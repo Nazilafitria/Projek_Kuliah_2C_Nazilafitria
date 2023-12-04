5bfe36475fc673bc45d3408d3ca8628b71e6ad9f
Nazilafitria <112499349+Nazilafitria@users.noreply.github.com>
2023-12-04 23:37:36 +0700

    Add files via upload

---
+++ Absensiku/index.php
@@ -0,0 +1,34 @@
+            <?php 
+            if(isset($_GET['x']) && $_GET['x']=='home'){
+                $page = "home.php";
+                include "main.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='santri'){
+                $page = "santri.php";
+                include "main.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='guru'){
+                $page = "guru.php";
+                include "main.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='kelas'){
+                $page = "kelas.php";
+                include "main.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='matapelajaran'){
+                $page = "matapelajaran.php";
+                include "main.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='jadwal'){
+                $page = "jadwal.php";
+                include "main.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='user'){
+                $page = "user.php";
+                include "main.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='rekapabsensi'){
+                $page = "rekapabsensi.php";
+                include "main.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='login'){
+                include "login.php";
+            }elseif(isset($_GET['x']) && $_GET['x']=='logout'){
+                include "proses/proses_logout.php";
+            }else{
+                $page = "home.php";
+                include "main.php";
+            }
+            ?>
